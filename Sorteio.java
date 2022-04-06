e55b44fe0467a3ef6a8df9982e5133385b1a9270
felipeschwebel <69486593+felipeschwebel@users.noreply.github.com>
2022-04-06 18:04:57 -0300

    Add files via upload

--- Sorteio.java
+++ Sorteio.java
@@ -1,48 +1,32 @@
 import java.util.Random;
 
-public class Sorteio { 
-    //Atributos , propriedades, variaveis globais
+public class Sorteio {
+    // Atributos, propriedades, variaveis globais
 
-    //funcoes e procedimentos
+    // Funcoes e procedimentos
 
     // Rotina principal
-
     public static void main(String[] args) {
-
         Random gerador = new Random();
-
         int c = 1;
-        int soma = 0; // ou 1
-        int maior = 0;
-        int menor = 100;
-
-       
-        while (c <= 1000){
-            //ler o numero
+        int soma = 0;
+        int maior = 0; // ou 1
+        int menor = 101; // ou 100
+        while (c <= 1000) {
+            // ler o numero
             int n = 1 + gerador.nextInt(100);
-
-
-           // if( c == 1){
-                //maior = n;
-                //menor = n;
-           // }
-            soma += n; //soma = soma + n
-            if (n > maior){
+            soma += n; // soma = soma + n
+            if (n > maior) {
                 maior = n;
-    
             }
-    
-            if (n > menor){
+            if (n < menor) {
                 menor = n;
-                
             }
-            
             c++; // c += 1 ou c = c + 1
             System.out.println(n);
         }
-        System.out.println("Maior = "+ maior);
-        System.out.println("Menor = "+ menor);
-        System.out.println("Soma = "+ soma/1000f);
-
+        System.out.println("Maior = " + maior); 
+        System.out.println("Menor = " + menor);
+        System.out.println("Média = " + soma / 1000f); // soma / (float) 1000;
     }
 }
